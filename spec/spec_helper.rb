bd0230c0224edf01ecfe50e107f6be2a7f1e7270
brianlaw033 <brian9328hk@gmail.com>
2017-04-10 11:07:06 +0800

    first commit=

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require("rspec")
+require("pg")
+require("movie")
+require("actor")
+
+DB = PG.connect({:dbname => "movie_database_test"})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM movies *;")
+    DB.exec("DELETE FROM actors *;")
+    DB.exec("DELETE FROM actors_movies *;")
+  end
+end
